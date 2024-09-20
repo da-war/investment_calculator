75ec8952f1eb4eb63e3c00ce95d2cf1708907430
drkast <ranadawarabdullah>
2024-09-21 01:14:35 +0500

    Types initialized

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 	var doPow = math.Pow(1+float64(expectedReturnRate)/100, float64(years))
