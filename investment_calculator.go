d2252faf9b1a0cfb5395ea1d1f6ddde3fab6f810
drkast <imdawar090@gmail.com>
2024-09-21 01:39:53 +0500

    This is github check

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 	doPow := math.Pow(1+expectedReturnRate/100, years)
 	futureValue := investmentAmount * doPow
-	fmt.Println(futureValue)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println("Future Value:", futureValue)
+	fmt.Println("Future Real Value:", futureRealValue)
+	fmt.Println("Working")
 }
