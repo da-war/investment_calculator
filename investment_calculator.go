3921db2945dab1036e1a78f4df87011bf8ceba2d
drkast <imdawar090@gmail.com>
2024-09-21 01:50:53 +0500

    A little cleaner code

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,11 +9,11 @@ func main() {
 	const inflationRate = 2.5
 	var investmentAmount, years, expectedReturnRate float64
 	//print in termial that you need to input the investment amount, years, and expected return rate
-	fmt.Println("Enter the investment amount: ")
+	fmt.Print("Enter the investment amount: ")
 	fmt.Scanln(&investmentAmount)
-	fmt.Println("Enter the number of years: ")
+	fmt.Print("Enter the number of years: ")
 	fmt.Scanln(&years)
-	fmt.Println("Enter the expected return rate: ")
+	fmt.Print("Enter the expected return rate: ")
 	fmt.Scanln(&expectedReturnRate)
 
 	doPow := math.Pow(1+expectedReturnRate/100, years)
@@ -22,5 +22,4 @@ func main() {
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println("Future Value:", futureValue)
 	fmt.Println("Future Real Value:", futureRealValue)
-	fmt.Println("Working")
 }
