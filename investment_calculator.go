bb93a4e252ee6383833f26596bc23f5f7ad01299
drkast <imdawar090@gmail.com>
2024-09-22 06:16:52 +0500

    Cal func

--- investment_calculator.go
+++ investment_calculator.go
@@ -19,13 +19,25 @@ func main() {
 	fmt.Print("Enter the expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	doPow := math.Pow(1+expectedReturnRate/100, years)
-	futureValue := investmentAmount * doPow
+	// doPow := math.Pow(1+expectedReturnRate/100, years)
+	// futureValue := investmentAmount * doPow
 
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Printf("The Future Value: %.2f\nThe Future Actual Value %.3f\n", futureValue, futureRealValue)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	// fmt.Printf("The Future Value: %.2f\nThe Future Actual Value %.3f\n", futureValue, futureRealValue)
+
+	fv, frv := calculateFutureValue(investmentAmount, years, expectedReturnRate, inflationRate)
+
+	fmt.Printf("The Future Value: %.2f\nThe Future Actual Value %.3f\n", fv, frv)
 }
 
 func stringGetter() string {
 	return "Welcome to the Investment Calculator"
+}
+
+func calculateFutureValue(investmentAmount, years, expectedReturnRate, inflationRate float64) (float64, float64) {
+	doPow := math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * doPow
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	return futureValue, futureRealValue
 }
